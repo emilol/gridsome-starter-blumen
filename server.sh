26d0899de4f46f9dc2acadb0cd894abf5586cd6b
Emily Taylor <emilytaylor@live.com.au>
2019-12-10 22:04:32 +1000

    fix prod script variable syntax

--- server.sh
+++ server.sh
@@ -1,11 +1,12 @@
 docker stop blumen
 docker container rm blumen
 
-volume = $GHOST_CONTENT_LOCAL_PATH/prod:/var/lib/ghost/content
-config = "su-exec node ghost config storage.cloudinary-store.cloud_name '$CLOUDINARY_CLOUD_NAME';\
+volume=$GHOST_CONTENT_LOCAL_PATH/prod:/var/lib/ghost/content
+
+config="su-exec node ghost config storage.cloudinary-store.cloud_name '$CLOUDINARY_CLOUD_NAME';\
     su-exec node ghost config storage.cloudinary-store.api_key '$CLOUDINARY_API_KEY';\
     su-exec node ghost config storage.cloudinary-store.api_secret '$CLOUDINARY_API_SECRET';\
     node current/index.js";
 
-lt --port 3001 --subdomain blumen &
-docker run --name blumen -p 3001:2368 -e url=https://blumen.localtunnel.me -v $volume emilol/docker-ghost-cloudinary bash -c "$config"
+lt --port 3001 --subdomain gridsome-starter-blumen &
+docker run --name blumen -p 3001:2368 -e url=https://gridsome-starter-blumen.localtunnel.me -v "$volume" emilol/docker-ghost-cloudinary bash -c "$config"
