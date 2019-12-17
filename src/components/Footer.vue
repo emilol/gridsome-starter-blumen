60f52db46a23e682aa3ec6d71e5f0a7a7e7217ea
Emily Taylor <emilytaylor@live.com.au>
2019-12-17 16:00:39 +1000

    fix footer link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="text-gray-700 text-sm leading-normal flex flex-wrap justify-between mx-auto max-w-3xl px-6 sm:px-12">
     <div class="w-full sm:w-1/2 mb-4 sm:mb-0">
-      <p><a href="https://github.com/cossssmin/gridsome-starter-bleda" class="border-b border-transparent hover:border-gray-400 transition-border-color">Blumen</a> by <a href="https://github.com/emilol" class="border-b border-transparent hover:border-gray-400 transition-border-color">@emilol</a></p>
+      <p><a href="https://github.com/emilol/gridsome-starter-blumen" class="border-b border-transparent hover:border-gray-400 transition-border-color">Blumen</a> by <a href="https://github.com/emilol" class="border-b border-transparent hover:border-gray-400 transition-border-color">@emilol</a></p>
       <p>Built with <a href="https://gridsome.org" class="border-b border-transparent hover:border-gray-400 transition-border-color">Gridsome</a> &amp; <a href="https://tailwindcss.com/" class="border-b border-transparent hover:border-gray-400 transition-border-color">Tailwind CSS</a>.</p>
     </div>
     <div class="w-full sm:w-1/2">
